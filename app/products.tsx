edfd32582589e22d1f687c46da01d740f161667e
Alan Asambaeve <alan.asambaevl@griffithuni.edu.au>
2026-05-12 17:17:13 +1000

    Milestone 2: Added shopping cart, num of items, total price

--- app/products.tsx
+++ app/products.tsx
@@ -1,5 +1,5 @@
 import { router, useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
@@ -10,20 +10,31 @@ import {
   View,
 } from "react-native";
 
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+};
+
 export default function ProductsScreen() {
-  const { category } = useLocalSearchParams();
-  const [products, setProducts] = useState<any[]>([]);
+  const { category } = useLocalSearchParams<{ category: string }>();
+
+  const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [category]);
 
-  const fetchProducts = async () => {
+  async function fetchProducts() {
     try {
+      setLoading(true);
+
       const response = await fetch(
         `https://fakestoreapi.com/products/category/${category}`,
       );
+
       const data = await response.json();
       setProducts(data);
     } catch (error) {
@@ -31,44 +42,50 @@ export default function ProductsScreen() {
     } finally {
       setLoading(false);
     }
-  };
-
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Products List</Text>
+      <View style={styles.box}>
+        <Text style={styles.header}>{category}</Text>
 
-      <FlatList
-        data={products}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Pressable
-            style={styles.card}
-            onPress={() =>
-              router.push({
-                pathname: "/product-details",
-                params: { id: item.id },
-              })
-            }
-          >
-            <Image source={{ uri: item.image }} style={styles.image} />
+        <View style={styles.contentBox}>
+          {loading ? (
+            <ActivityIndicator size="large" />
+          ) : (
+            <FlatList
+              data={products}
+              keyExtractor={(item) => item.id.toString()}
+              showsVerticalScrollIndicator
+              renderItem={({ item }) => (
+                <Pressable
+                  style={styles.card}
+                  onPress={() =>
+                    router.push({
+                      pathname: "/product-details",
+                      params: { id: item.id.toString() },
+                    })
+                  }
+                >
+                  <Image source={{ uri: item.image }} style={styles.image} />
 
-            <View style={styles.info}>
-              <Text numberOfLines={2} style={styles.title}>
-                {item.title}
-              </Text>
-              <Text style={styles.price}>${item.price}</Text>
-            </View>
-          </Pressable>
-        )}
-      />
+                  <View style={styles.info}>
+                    <Text style={styles.title} numberOfLines={2}>
+                      {item.title}
+                    </Text>
+
+                    <Text style={styles.price}>Price: ${item.price}</Text>
+                  </View>
+                </Pressable>
+              )}
+            />
+          )}
+        </View>
+
+        <Pressable style={styles.backButton} onPress={() => router.back()}>
+          <Text style={styles.backText}>Back</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -76,46 +93,79 @@ export default function ProductsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-  },
-  center: {
-    flex: 1,
+    backgroundColor: "#eeeeee",
     justifyContent: "center",
     alignItems: "center",
   },
-  header: {
-    backgroundColor: "#3fa7d6",
-    color: "white",
-    textAlign: "center",
-    fontWeight: "bold",
-    padding: 12,
-    borderRadius: 4,
-    marginBottom: 16,
+
+  box: {
+    width: "90%",
+    height: "90%",
+    backgroundColor: "white",
+    padding: 10,
+    borderWidth: 2,
+    borderColor: "#222",
   },
+
+  header: {
+    textAlign: "center",
+    backgroundColor: "#3fa0cf",
+    color: "white",
+    padding: 10,
+    fontWeight: "bold",
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: "#333",
+    textTransform: "capitalize",
+  },
+
+  contentBox: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: "#333",
+    padding: 10,
+  },
+
   card: {
     flexDirection: "row",
-    borderWidth: 1,
-    borderColor: "#999",
+    borderWidth: 2,
+    borderColor: "#666",
     padding: 10,
-    marginBottom: 12,
-    borderRadius: 6,
+    marginBottom: 10,
+    alignItems: "center",
     backgroundColor: "white",
   },
+
   image: {
     width: 70,
     height: 70,
     resizeMode: "contain",
-    marginRight: 12,
+    marginRight: 10,
   },
+
   info: {
     flex: 1,
   },
+
   title: {
+    fontSize: 12,
     fontWeight: "bold",
+    marginBottom: 8,
   },
+
   price: {
-    marginTop: 8,
-    color: "green",
+    fontSize: 12,
+  },
+
+  backButton: {
+    backgroundColor: "#1976d2",
+    padding: 10,
+    alignItems: "center",
+    marginTop: 10,
+  },
+
+  backText: {
+    color: "white",
     fontWeight: "bold",
   },
 });
